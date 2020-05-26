7e8b84096960ab186229edb1db207e95e3d6fa5c
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-05-26 13:58:17 +0530

    Add files via upload

---
+++ Java Basic 1/InchesToMeters.java
@@ -0,0 +1,22 @@
+// To convert length from inches to meters
+
+import java.util.Scanner;
+
+class Length{
+
+	public static void main(String[] args){
+
+		double inch,meter;
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Input a value for inch:");
+		inch = s.nextDouble();
+
+		meter = inch*0.0254;	// Formula for conversion
+
+		System.out.println(inch+" inch is "+meter+"meters");
+	}
+}
+~                                                                                                                                                                      
+~                                                                                                                                                                      
+~                                                                                                                                                                                
