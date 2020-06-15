db49ffa43dd2adb942a3d9187f0e9ec08fb4010f
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-06-15 23:03:36 +0530

    Add files via upload

---
+++ Java Basic 2/FirstTenNaturalNumbers.java
@@ -0,0 +1,18 @@
+// To Print the first ten natural numbers
+
+class FirstTenNaturalNumbers{
+
+	public static void main(String[] args){
+
+		int n=1,i;
+
+		for(i=1;i<=10;i++){	//loop to print the natural numbers n times
+
+			System.out.println(n);
+			n++;
+
+		}	
+
+	}
+
+} 
