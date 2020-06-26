f6de3df3ea85bd2f1e96bf46deccfa9c4c3fb4b4
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-06-26 22:07:28 +0530

    Add files via upload

---
+++ Java Basic 2/GridDesign.java
@@ -0,0 +1,19 @@
+// To make a grid design using '-' 
+
+
+class GridDesign{
+
+	public static void main(String[] args){
+
+		int i,j;
+
+		for(i=0;i<=10;i++){	// i loop till any value depending on size
+
+			for(j=0;j<=30;j++){	// j loop till any size too
+
+				System.out.print("-");	// element to print
+			}	 
+			System.out.println("");	// breaking to next line
+		}
+	}
+}
